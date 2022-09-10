5ee73d42dbcd8550dc884df33a6c6a7e48ddb2f8
jlmayo <108962022+jlmayo@users.noreply.github.com>
2022-09-10 14:55:37 -0700

    Jumbotron works. Hourly colors show up.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 var today = moment();
 $('#currentDay').text(today.format('MMMM Do, YYYY'));
 
+
+var hourCycle =function(){
+    var currentHour = moment().hour() 
+
+    for(var i = 9; i < 18; i++){
+        var taskArea = $("#task-"+i)  
+        if(currentHour > i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        }else{
+            $(taskArea).addClass("future")
+        }
+    }
+}
+
+hourCycle();
