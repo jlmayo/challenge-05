ced73c3b3e224ba1539ee04aef3b949b011fac41
jlmayo <108962022+jlmayo@users.noreply.github.com>
2022-09-10 16:04:31 -0700

    JS Done! It works!

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Inserts Current Day into Jumbotron
 var today = moment();
 $('#currentDay').text(today.format('MMMM Do, YYYY'));
 
 
+//Applies Current Hour and CSS styling based on past, present or furture time
 var hourCycle =function(){
     var currentHour = moment().hour() 
 
@@ -19,3 +21,27 @@ var hourCycle =function(){
 
 hourCycle();
 
+//Applies functionality to Save Button so that text entry is saved to Local Storage and entries persist.
+var saveBtn = $('.saveBtn');
+
+saveBtn.on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    var task = $(this).siblings(".taskEntry").val();
+
+    localStorage.setItem(time, task);
+});
+
+
+
+function saveTasks () {
+    $(".hour").each(function() {
+        var rightNow = $(this).text();
+        var myTask = localStorage.getItem(rightNow);
+
+        if (myTask !== null) {
+            $(this).siblings(".taskEntry").val(myTask);
+        }
+    });
+}
+
+saveTasks();
